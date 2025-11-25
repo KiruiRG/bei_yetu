05f80a22c080a3d843fd2973c8e66ee00fc6b8ab
LMaithya <laura.maithya@strathmore.edu>
2025-11-25 09:52:32 +0300

    It is getting worse

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,6 +82,8 @@ android {
         implementation("androidx.compose.ui:ui-tooling-preview:1.6.0")
         debugImplementation("androidx.compose.ui:ui-tooling:1.6.0")
         implementation("com.google.android.material:material:1.11.0")
+        implementation("androidx.navigation:navigation-compose:2.7.0")
+
 
     }
 
